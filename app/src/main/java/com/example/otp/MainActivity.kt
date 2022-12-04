45239d5662d0f8df31638400cc8a0ab9fd7930ba
Kishan Viramgama <viramgama.kishan@gmail.com>
2022-12-04 22:16:55 +0530

    Work with LazyRow

--- app/src/main/java/com/example/otp/MainActivity.kt
+++ app/src/main/java/com/example/otp/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.library.Otp
 import com.example.otp.ui.theme.OtpTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Otp()
                 }
             }
         }
@@ -31,7 +33,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    //Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
--- library/src/main/java/com/example/library/Otp.kt
+++ library/src/main/java/com/example/library/Otp.kt
@@ -1,5 +1,47 @@
 package com.example.library
 
-import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 
-class Otp : ComponentActivity() {}
+@ExperimentalMaterial3Api
+@Composable
+fun Otp() {
+
+    LazyRow(
+        modifier = Modifier.padding(start = 10.dp, top = 10.dp, end = 10.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        items(5) { item ->
+            Box(modifier = Modifier.width(60.dp)) {
+                var stringText by remember { mutableStateOf(TextFieldValue("")) }
+                OutlinedTextField(
+                    value = stringText,
+                    onValueChange = {
+                        if (it.text.isDigitsOnly()) {
+                            stringText= it
+                        }
+                    },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
+                    modifier = Modifier.padding(end = 10.dp)
+                )
+
+            }
+        }
+    }
+
+}
