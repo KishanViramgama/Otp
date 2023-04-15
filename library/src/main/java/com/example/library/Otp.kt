157804c7f44c0404c46c745b34aa583bfd749669
KishanViramgama <viramgama.kishan@gmail.com>
2023-04-15 23:25:26 +0530

    Work with FlowRow

--- library/src/main/java/com/example/library/Otp.kt
+++ library/src/main/java/com/example/library/Otp.kt
@@ -2,6 +2,8 @@ package com.example.library
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
@@ -17,30 +19,167 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
 
+@OptIn(ExperimentalLayoutApi::class)
 @ExperimentalMaterial3Api
 @Composable
 fun Otp() {
 
-    LazyRow(
+    FlowRow(
         modifier = Modifier.padding(start = 10.dp, top = 10.dp, end = 10.dp),
         horizontalArrangement = Arrangement.Center
     ) {
-        items(5) { item ->
-            Box(modifier = Modifier.width(60.dp)) {
-                var stringText by remember { mutableStateOf(TextFieldValue("")) }
-                OutlinedTextField(
-                    value = stringText,
-                    onValueChange = {
-                        if (it.text.isDigitsOnly()) {
-                            stringText= it
-                        }
-                    },
-                    singleLine = true,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
-                    modifier = Modifier.padding(end = 10.dp)
-                )
-
-            }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+        Box(modifier = Modifier.width(60.dp)) {
+            var stringText by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(
+                value = stringText,
+                onValueChange = {
+                    if (it.text.isDigitsOnly()) {
+                        stringText = it
+                    }
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.padding(end = 10.dp)
+            )
         }
     }
 
