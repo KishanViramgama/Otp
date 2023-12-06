69eca8b48bd9d89177c821aaeb96ac3bf32bd56f
KishanViramgama <viramgama.kishan@gmail.com>
2023-12-06 23:10:20 +0530

    Working on logic

--- library/src/main/java/com/example/library/MyOtp.kt
+++ library/src/main/java/com/example/library/MyOtp.kt
@@ -1,6 +1,5 @@
 package com.example.library
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
